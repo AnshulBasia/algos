beab33c7fcf3138be68aa0918d99ae7c190b0203
Anshul Basia <anshul.dbgt@gmail.com>
2016-09-08 19:32:04 +0530

    check prefix

--- bad_trie_check.cpp
+++ bad_trie_check.cpp
@@ -4,10 +4,11 @@ using namespace std;
 struct node
 {
 	struct node * parent;
-	struct node* children[25];
+	struct node* children[11];
 	vector<int> occurrences;
 	char ch;
 	int no;
+	bool terminated;
 };
 
 void InsertWord(struct node *trieTree,char *word,int index)
@@ -21,20 +22,24 @@ void InsertWord(struct node *trieTree,char *word,int index)
 		{
 			traverse->children[(int)*word-97]=(struct node*)calloc(1,sizeof(struct node));
 			traverse->children[(int)*word-97]->no=0;
+			traverse->children[(int)*word-97]->terminated=false;
 			traverse->children[(int)*word-97]->parent=traverse;
 			traverse->children[(int)*word-97]->ch=*word;
 		}
 
 		
 		traverse->no++;
+		
 		//cout<<traverse->no<<" "<<traverse->ch<<endl;
 		traverse=traverse->children[(int)*word-97];
 		++word;
+		//cout<<"inserting "<<traverse->terminated<<" "<<traverse->ch<<endl;
 	}
 	traverse->no++;	
 	
 	traverse->occurrences.push_back(index);
-	
+	traverse->terminated=true;
+	//cout<<"inserting "<<traverse->terminated<<" "<<traverse->ch<<endl;
 	
 }
 
@@ -66,19 +71,36 @@ int SearchWord(struct node* trieTree,char* word)
     return traverse->no;
 }
 
+bool check_if_bad(struct node* tree,char *word)
+{
+	struct node* traverse=tree;
+	while(*word!='\0')
+	{
+		traverse=traverse->children[(int)*word-97];
+		++word;
+		//cout<<"no "<<traverse->no<<" "<<traverse->terminated<<" "<<traverse->ch<<endl;
+	
+		if(traverse->no>1&&traverse->terminated==true)
+		{
+			return true;
+		}
+	}
+	//
+	return false;
+}
 int main()
 {
 	int t;
 	cin>>t;
-	
+	string ans;
+	int flag=0;
 	node *tree=(struct node*)calloc(1,sizeof(struct node));
 
 	while(t--)
 	{	
-		string s,task;
-		cin>>task;
+		string s;
 		cin>>s;
-		char a[25];
+		char a[65];
 		int i;
 		
 		for(i=0;i<s.length();i++)
@@ -89,21 +111,22 @@ int main()
 			
 		}
 		a[i]='\0';
-		
-		if(task[0]=='a')  //add a word
+		InsertWord(tree,a,t);
+		if(check_if_bad(tree,a)==1&&flag==0)
 		{
-			
-			
-			InsertWord(tree,a,t);
-			//cout<<"INSERTED"<<endl;
-		}
-		
-		if(task[0]=='f')
-		{
-			//cout<<"FINDING "<<a<<" "<<endl;
-
-			cout<<SearchWord(tree,a)<<endl;
+			ans=a;
+			flag=1;
 		}
 	}
+
+	if(flag==0){cout<<"GOOD SET"<<endl;}
+	else
+	{
+		cout<<"BAD SET"<<endl;
+		cout<<ans<<endl;
+	}
+
+	
+
 	
 }

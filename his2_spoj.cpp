c7115105270f3d2ad0b1faba6c72f1e9a26d9c5c
Anshul Basia <anshul.dbgt@gmail.com>
2016-09-13 01:18:52 +0530

    histogram

---
+++ his2_spoj.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <stdexcept> // out_of_range exception
+
+int main()
+{
+	int n;
+	int mask;
+	int heights[100];
+	vector<int> p;
+	int index,maxx;
+	while(1>0)
+	{
+		cin>>n;
+		if(n==0){break;}
+		mask=0;
+		for(int i=0;i<n;i++)
+		{
+			cin>>heights[i];
+		}
+		p.push_back(heights[0]);
+		int j=1;
+		while(p.size()<n)
+		{
+			maxx=0;
+			index=0;
+			for(int i=0;i<p.size()+1;i++)
+			{	
+				if(i==0)
+				{
+					if(maxx<heights[j]+abs(heights[j]-p[i]))
+					{
+						maxx=heights[j]+abs(heights[j]-p[i]);
+						index=i;
+					}
+				
+				}
+				if(i==p.size())
+				{
+					if(maxx<heights[j]+abs(heights[j]-p[i-1]))
+					{
+						maxx=heights[j]+abs(heights[j]-p[i-1]);
+						index=i;
+					}
+				
+				}
+
+				if(i!=0&&i!=p.size())
+				{	if(maxx<abs(heights[j]-p[i-1])+abs(heights[j]-p[i]))
+					{
+						maxx=abs(heights[j]-p[i-1])+abs(heights[j]-p[i]);
+						index=i;
+					}
+				}	
+			}
+			//cout<<index<<" "<<heights[j]<<endl;
+			//cout<<maxx<<endl;
+			p.insert(p.begin()+index,heights[j]);
+			/*
+			vector<int> temp;
+			for(int i=0;i<index;i++){temp.push_back(p[i]);}
+			temp.push_back(heights[j]);
+			for(int i=index;i<p.size();i++){temp.push_back(p[i]);}
+
+			p=temp;
+			temp.clear();
+			*/
+			j++;
+		}
+		for(int i=0;i<p.size();i++){cout<<p[i]<<" ";}
+			cout<<endl;
+		p.clear();
+	}
+}

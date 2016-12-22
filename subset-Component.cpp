c5963e5372bbb0f7086448806b6e6ded25965ed3
Anshul Basia <mt6140502@iitd.ac.in>
2016-12-23 00:55:11 +0530

    rank and compression

--- subset-Component.cpp
+++ subset-Component.cpp
@@ -7,34 +7,46 @@
 using namespace std;
 
 int parent[64];
+int rankk[64];
 int find(int parent[],int x){
-    if(parent[x]==-1){return x;}
+    if(parent[x]==x){return x;}
     return find(parent,parent[x]);
 }
 
-void unionn(int parent[],int x,int y){
+void unionn(int parent[],int rankk[],int x,int y){
     if(x==y){return;}
     int a=find(parent,x);
     int b=find(parent,y);
-    parent[a]=b;
+    if(rankk[a]<rankk[b]){
+        parent[a]=b;
+    }
+    else if(rankk[a]>rankk[b]){
+        parent[b]=a;
+    }
+    else{
+        parent[a]=b;
+        rankk[b]++;
+    }
 }
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
     cin>>n;
-    long long d[24],temp;
+    unsigned long long d[24],temp;
     for(int i=0;i<n;i++){cin>>d[i];}
-    long long mask = (1<<n);
+    unsigned long long mask = (1<<n);
     long long count,total=0;
-    
     bool visited[64];
     for(long long i=0;i<mask;i++){
-        memset(&parent,-1,sizeof(parent));
+        memset(&rankk,0,sizeof(rankk));
+        for(int r=0;r<64;r++){
+            parent[r]=r;
+        }
         for(int j=0;j<n;j++){
             if((i>>j)==0){break;}
             if((i>>j)&1){
                 //d[j] is present in the subset
-             
+               
                 int t=0;
                 for(int k=0;k<64;k++){
                     if((d[j]>>k)&1){
@@ -44,9 +56,11 @@ int main() {
                 }
                 
                 for(int k=t+1;k<64;k++){
+                    
                     if((d[j]>>k)&1){
-                       
-                        unionn(parent,t,k);
+                       // cout<<t<<" "<<k<<endl;
+                        unionn(parent,rankk,t,k);
+                        
                     }
                 }
             }
@@ -56,18 +70,11 @@ int main() {
         int temp;
         memset(&visited,false,sizeof(visited));
         for(int q=0;q<64;q++){
-            if(!visited[q]){
-                visited[q]=true;
-                temp=q;
-                while(parent[q]!=-1){
-                    q=parent[q];
-                    visited[q]=true;
-                }
-                q=temp;
+            if(!visited[parent[q]]){
+                visited[parent[q]]=true;
                 count++;
             }
         }
-        //cout<<i<<" "<<count<<endl;
         total+=count;
     }
     cout<<total<<endl;
